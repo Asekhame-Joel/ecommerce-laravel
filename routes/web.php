78f682c90e297e6d161c691b28e3f4a931b20892
Asekhame Joel <asekhamejoel@gmail.com>
2025-12-24 09:09:49 +0100

    feat: Integrate new frontend template and assets.

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.home.index');
 });
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
